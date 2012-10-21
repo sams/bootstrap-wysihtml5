f9db8dbb93b6487620ddc461ced108f672ce298d
Sam Sherlock <sam.sherlock@gmail.com>
2012-10-21 12:30:35 +0100

    update src js of bootstrap-wysihtml5; add html true and custom style to demo

--- src/bootstrap-wysihtml5.js
+++ src/bootstrap-wysihtml5.js
@@ -271,6 +271,7 @@
             var insertImage = function() {
                 var url = urlInput.val();
                 urlInput.val(initialValue);
+                self.editor.currentView.element.focus();
                 self.editor.composer.commands.exec("insertImage", url);
             };
 
@@ -317,6 +318,7 @@
             var insertLink = function() {
                 var url = urlInput.val();
                 urlInput.val(initialValue);
+                self.editor.currentView.element.focus();
                 self.editor.composer.commands.exec("createLink", {
                     href: url,
                     target: "_blank",

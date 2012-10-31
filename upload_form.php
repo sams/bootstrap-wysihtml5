4c933a75c5cfbf73844b532a4eff9a3a8b582ce8
Sam Sherlock <sam.sherlock@gmail.com>
2012-10-31 21:04:51 +0000

    an upload form for now

---
+++ upload_form.php
@@ -0,0 +1,21 @@
+<?php
+
+?>
+<!html>
+<html>
+    <body>
+        <form action="upload.php" method="post" enctype="multipart/form-data">
+           <p>
+              <label for="file">Select a file:</label> <input type="file" name="Filedata" id="file"> <br />
+              <button>Upload File</button>
+           </p>
+        </form>
+        <form action="upload.php" method="post" enctype="multipart/form-data">
+           <p>
+              <label for="file">Select a file:</label> <input type="file" name="Filedata" id="file"> <br />
+              <button>Upload File</button>
+           </p>
+        </form>
+    </body>
+</html>
+
